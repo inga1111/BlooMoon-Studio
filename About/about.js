130802b863f4befe049f3c2e7e37b4741c07eda3
inga1111 <2582423@students.wits.ac.za>
2025-11-06 15:35:32 +0200

    about pages

---
+++ About/about.js
@@ -0,0 +1,25 @@
+// GSAP title animation
+gsap.from(".about-title", { opacity: 0, y: -40, duration: 1.3, ease: "power2.out" });
+gsap.from(".about-subtitle", { opacity: 0, y: -20, duration: 1.1, delay: 0.3, ease: "power2.out" });
+
+// Animate cards on scroll
+gsap.utils.toArray(".card").forEach((card, i) => {
+  gsap.from(card, {
+    scrollTrigger: {
+      trigger: card,
+      start: "top 85%",
+    },
+    opacity: 0,
+    y: 60,
+    duration: 1,
+    delay: i * 0.1,
+    ease: "power2.out",
+  });
+});
+
+// Load nav and footer
+fetch("../main.js")
+  .then(() => {
+    if (typeof loadNavBar === "function") loadNavBar();
+    if (typeof loadFooter === "function") loadFooter();
+  });
